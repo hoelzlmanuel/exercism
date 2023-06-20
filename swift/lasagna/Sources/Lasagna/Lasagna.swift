841c8770a8983562edf21f8d6b270bfb0157a86c
Manuel Hölzl <hoelzl1912@gmail.com>
2023-06-21 00:05:15 +0200

    solve 'Hello World' and 'Layers of Lasagne' Swift exercises

---
+++ swift/lasagna/Sources/Lasagna/Lasagna.swift
@@ -0,0 +1,13 @@
+let expectedMinutesInOven = 40
+
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    expectedMinutesInOven - elapsedMinutes
+}
+
+func preparationTimeInMinutes(layers: Int) -> Int {
+    2 * layers
+}
+
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    preparationTimeInMinutes(layers: layers) + elapsedMinutes
+}
